45b503c2752988fba14dd44c8b7b30de1005e735
Boris <Zelyanin>
2022-08-11 12:35:25 +0300

    sprint 5-6

--- internal/model/Order.go
+++ internal/model/Order.go
@@ -12,7 +12,7 @@ const (
 type DataOrder struct {
 	Number     string    `json:"number"`
 	Status     string    `json:"status"`
-	Accrual    *float64  `json:"accrual,omitempty"` //nullable
+	Accrual    float32   `json:"accrual,omitempty"` //nullable
 	Person     string    `json:"-"`
 	UploadedAt time.Time `json:"uploaded_at"`
 }
