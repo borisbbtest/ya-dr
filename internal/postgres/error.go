96b0a2671564c27d8b6cade85443bcf1e4b3a79f
Boris <Zelyanin>
2022-08-16 15:53:57 +0300

    yes

--- internal/postgres/error.go
+++ internal/postgres/error.go
@@ -3,41 +3,13 @@ package postgres
 import (
 	"fmt"
 	"strings"
-
-	"golang.org/x/text/cases"
-	"golang.org/x/text/language"
 )
 
 type Error string
 
 func (e Error) Error() string { return string(e) }
 
-const (
-	errorTooManyParameters               = Error("Too many parameters.")
-	errorCannotFetchData                 = Error("Cannot fetch data.")
-	errorPostgresPing                    = Error("PostgreSQL ping failed: got wrong data.")
-	errorCannotParseData                 = Error("Cannot parse data.")
-	errorUnsupportedQuery                = Error("Unsupported query.")
-	errorEmptyResult                     = Error("Empty result.")
-	errorCannotConvertPostgresVersionInt = Error("Cannot convert Postgres version to integer.")
-	errorFourthParamEmpty                = Error("The key requires database name as fourth parameter")
-	errorFourthParamLen                  = Error("Expected database name as fourth parameter for the key, got empty string")
-	errorUnknownSession                  = Error("Unknown session")
-)
-
-// formatError formats a given error text. It capitalizes the first letter and adds a dot to the end.
-// TBD: move to the agent's core
-func formatError(errText string) string {
-	if errText[len(errText)-1:] != "." {
-		errText = errText + "."
-	}
-	return cases.Title(language.Und, cases.NoLower).String(errText)
-}
-
 func sanitizeError(errText, connString string) (err error) {
-	// default error string shows user credentials used to establish connection
-	// this can be potentialy dangerous
-	// create error message string without sensitive info
 	var (
 		originSchema, sanitizedErrorString string
 		startingIndexOfError               int
--- internal/postgres/postgres.go
+++ internal/postgres/postgres.go
@@ -24,39 +24,6 @@ func (p *Plugin) Stop() {
 	p.connMgr = nil
 }
 
-// whereToConnect builds a session based on key's parameters and a configuration file.
-func whereToConnect(params []string) (u *URI, err error) {
-	var uri string
-	user := ""
-	if len(params) > 1 {
-		user = params[1]
-	}
-
-	password := ""
-	if len(params) > 2 {
-		password = params[2]
-	}
-
-	database := ""
-	if len(params) > 3 && len(params) < 5 {
-		database = params[3]
-	}
-
-	// The first param can be either a URI or a session identifier
-	if len(params) > 0 && len(params[0]) > 0 {
-		if isLooksLikeURI(params[0]) {
-			// Use the URI defined as key's parameter
-			uri = params[0]
-		}
-	}
-
-	if len(user) > 0 || len(password) > 0 || len(database) > 0 {
-		return newURIWithCreds(uri, user, password, database)
-	}
-
-	return parseURI(uri)
-}
-
 func (p *Plugin) NewConn() (conn *postgresConn, err error) {
 
 	conn, err = p.connMgr.GetPostgresConnection(p.dsn)
@@ -65,40 +32,4 @@ func (p *Plugin) NewConn() (conn *postgresConn, err error) {
 		return nil, err
 	}
 	return
-}
-
-// Export implements the Exporter interface.
-func (p *Plugin) NewDBConn(key string, params []string, dsnString string, handlerParams []interface{}) (result interface{}, err error) {
-	var (
-		handler requestHandler
-	)
-	var connString string
-	u, err := whereToConnect(params)
-	if err != nil {
-		connString = dsnString
-	} else {
-		// get connection string for PostgreSQL
-		connString = u.URI()
-	}
-	switch key {
-	case keyPostgresPing:
-		handler = p.pingHandler // postgres.ping[[connString]]
-	case keyPostgresCreateDdLoyaltySystem:
-		handler = p.CreateTableLoyaltySystemHandler
-	default:
-		return nil, errorUnsupportedQuery
-	}
-
-	conn, err := p.connMgr.GetPostgresConnection(connString)
-	if err != nil {
-		// Here is another logic of processing connection errors if postgres.ping is requested
-		if key == keyPostgresPing {
-			return postgresPingFailed, nil
-		}
-		log.Errorf("connection error: %s", err)
-		log.Debugf("parameters: %+v", params)
-		return nil, err
-	}
-
-	return handler(conn, key, handlerParams)
 }
