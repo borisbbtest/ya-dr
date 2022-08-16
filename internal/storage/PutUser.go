ee0bc3440772408970d460bc50579bb62bce0fd8
Boris <Zelyanin>
2022-08-16 15:13:54 +0300

    yes

--- internal/storage/PutUser.go
+++ internal/storage/PutUser.go
@@ -1,15 +1,39 @@
 package storage
 
-import "github.com/borisbbtest/ya-dr/internal/model"
+import (
+	"context"
+
+	"github.com/borisbbtest/ya-dr/internal/model"
+)
 
 func (hook *StoreDBinPostgreSQL) PutUser(v model.DataUser) (string, error) {
-	buff := []interface{}{v.Login, v.Password}
-	res, err := hook.pgp.NewDBConn("pgsql.insert.tb.user", []string{}, hook.connStr, buff)
+	var err error
+	var users string
+
+	conn, err := hook.pgp.NewConn()
 	if err != nil {
-		log.Info(res)
-		hook.pgp.NewDBConn("pgsql.insert.tb.balance", []string{}, hook.connStr, buff)
+		log.Error("selectOrdersHandler - c: ", err)
 		return "", err
 	}
 
-	return res.(string), err
+	query := `
+	WITH cte AS (
+		INSERT INTO public."Users"(
+		"Login", "Password" )
+		VALUES ($1, $2)
+		ON CONFLICT ("Login") DO NOTHING
+		RETURNING "Login"
+	)
+	SELECT NULL AS result
+	WHERE EXISTS (SELECT 1 FROM cte)
+	UNION ALL
+    SELECT "Login"  FROM  "Users"  WHERE  "Login"  = $1;`
+
+	err = conn.PostgresPool.QueryRow(context.Background(), query, v.Login, v.Password).Scan(&users)
+
+	if err != nil {
+		log.Info("insertUserHandler  --  ", err)
+		return "", err
+	}
+	return users, nil
 }
