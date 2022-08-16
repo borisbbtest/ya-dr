7f1259db86af7ddca82ddbaa213973c9de0e0acf
Boris <Zelyanin>
2022-08-16 15:01:26 +0300

    yes

--- internal/postgres/postgres.go
+++ internal/postgres/postgres.go
@@ -87,8 +87,6 @@ func (p *Plugin) NewDBConn(key string, params []string, dsnString string, handle
 		handler = p.CreateTableLoyaltySystemHandler
 	case keyPostgresInsertUser:
 		handler = p.insertUserHandler
-	case keyPostgresSelectUser:
-		handler = p.selectUserHandler
 	case keyPostgresInsertOrder:
 		handler = p.insertOrderHandler
 	case keyPostgresUpdateOrder:
--- internal/storage/GetUser.go
+++ internal/storage/GetUser.go
@@ -1,15 +1,24 @@
 package storage
 
-import "github.com/borisbbtest/ya-dr/internal/model"
+import (
+	"context"
+
+	"github.com/borisbbtest/ya-dr/internal/model"
+)
 
 func (hook *StoreDBinPostgreSQL) GetUser(k model.DataUser) (model.DataUser, error) {
-
-	buff := []interface{}{k.Login}
-	res, err := hook.pgp.NewDBConn("pgsql.select.tb.user", []string{}, hook.connStr, buff)
+	buff := model.DataUser{}
+	query := `SELECT "Id", "Login",  "Password"  FROM  "Users"  WHERE  "Login"  = $1;`
+	conn, err := hook.pgp.NewConn()
 	if err != nil {
-		log.Error("pgsql.select.tb.user", err)
+		log.Error("selectOrdersHandler - c: ", err)
+		return model.DataUser{}, err
+	}
+	err = conn.PostgresPool.QueryRow(context.Background(), query, k.Login).Scan(&buff.ID, &buff.Login, &buff.Password)
+	if err != nil {
+		log.Error("selectUserHandler", err)
 		return model.DataUser{}, err
 	}
 
-	return res.(model.DataUser), nil
+	return buff, nil
 }
