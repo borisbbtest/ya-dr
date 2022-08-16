30058815ab81085f0f066fc62d25e67c90e733a2
Boris <Zelyanin>
2022-08-16 15:28:16 +0300

    yes

--- internal/storage/PutOrder.go
+++ internal/storage/PutOrder.go
@@ -1,12 +1,36 @@
 package storage
 
-import "github.com/borisbbtest/ya-dr/internal/model"
+import (
+	"context"
+
+	"github.com/borisbbtest/ya-dr/internal/model"
+)
 
 func (hook *StoreDBinPostgreSQL) PutOrder(v model.DataOrder) (int, error) {
-	buff := []interface{}{v.Number, v.Status, v.Person}
-	res, err := hook.pgp.NewDBConn("pgsql.insert.tb.order", []string{}, hook.connStr, buff)
+	var err error
+	var person int
+	query := `
+	WITH cte AS (
+		INSERT INTO public."Orders" ("Number","Status", "Person","Uploaded_at")
+		VALUES ($1, $2, $3, NOW())
+		ON CONFLICT ("Number") DO NOTHING
+		RETURNING "Number"
+	)
+	SELECT NULL AS result
+	WHERE EXISTS (SELECT 1 FROM cte)
+	UNION ALL
+    SELECT  "Person"  FROM  "Orders"  WHERE  "Number"  = $1;`
+	conn, err := hook.pgp.NewConn()
 	if err != nil {
+		log.Error("selectOrdersHandler - c: ", err)
 		return -1, err
 	}
-	return res.(int), err
+	err = conn.PostgresPool.QueryRow(context.Background(), query, v.Number, v.Status, v.Person).Scan(&person)
+
+	if err != nil {
+		log.Info("insertOrderHandler  --  ", err)
+		return -1, err
+	}
+
+	return person, nil
 }
