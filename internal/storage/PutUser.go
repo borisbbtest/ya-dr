7c022952d6e8306391a09aac84a689209470e2b5
Boris <Zelyanin>
2022-08-16 15:21:32 +0300

    yes

--- internal/storage/PutUser.go
+++ internal/storage/PutUser.go
@@ -1,57 +1,48 @@
 package storage
 
 import (
+	"context"
+
 	"github.com/borisbbtest/ya-dr/internal/model"
 )
 
 func (hook *StoreDBinPostgreSQL) PutUser(v model.DataUser) (string, error) {
-	buff := []interface{}{v.Login, v.Password}
-	res, err := hook.pgp.NewDBConn("pgsql.insert.tb.user", []string{}, hook.connStr, buff)
+
+	var err error
+	var users string
+
+	conn, err := hook.pgp.NewConn()
 	if err != nil {
-		log.Info(res)
-		hook.pgp.NewDBConn("pgsql.insert.tb.balance", []string{}, hook.connStr, buff)
+		log.Error("selectOrdersHandler - c: ", err)
 		return "", err
 	}
 
-	return res.(string), err
+	query := `
+	WITH cte AS (
+		INSERT INTO public."Users"(
+		"Login", "Password" )
+		VALUES ($1, $2)
+		ON CONFLICT ("Login") DO NOTHING
+		RETURNING "Login"
+	)
+	SELECT NULL AS result
+	WHERE EXISTS (SELECT 1 FROM cte)
+	UNION ALL
+	SELECT "Login"  FROM  "Users"  WHERE  "Login"  = $1;`
 
-	// var err error
-	// var users string
+	err = conn.PostgresPool.QueryRow(context.Background(), query, v.Login, v.Password).Scan(&users)
 
-	// conn, err := hook.pgp.NewConn()
-	// if err != nil {
-	// 	log.Error("selectOrdersHandler - c: ", err)
-	// 	return "", err
-	// }
+	if err != nil {
+		log.Info("insertUserHandler  --  ", err)
 
-	// query := `
-	// WITH cte AS (
-	// 	INSERT INTO public."Users"(
-	// 	"Login", "Password" )
-	// 	VALUES ($1, $2)
-	// 	ON CONFLICT ("Login") DO NOTHING
-	// 	RETURNING "Login"
-	// )
-	// SELECT NULL AS result
-	// WHERE EXISTS (SELECT 1 FROM cte)
-	// UNION ALL
-	// SELECT "Login"  FROM  "Users"  WHERE  "Login"  = $1;`
+		query = `INSERT INTO "Balance" ("Person", "Withdrawn" , "CurrentAccrual" )
+				 SELECT "Id", 0 ,0  From "Users" WHERE  "Login"  = $1 ;`
 
-	// err = conn.PostgresPool.QueryRow(context.Background(), query, v.Login, v.Password).Scan(&users)
+		if _, err := conn.PostgresPool.Exec(context.Background(), query, v.Login); err != nil {
+			log.Info("insertBalanceHandler --- ", err)
+		}
+		return "", err
+	}
 
-	// if err != nil {
-	// 	log.Info("insertUserHandler  --  ", err)
-	// 	return "", err
-	// }
-
-	// return users, nil
-
-	// query = `INSERT INTO "Balance" ("Person", "Withdrawn" , "CurrentAccrual" )
-	// SELECT "Id", 0 ,0  From "Users" WHERE  "Login"  = $1 ;`
-
-	// if _, err := conn.PostgresPool.Exec(context.Background(), query, v.Login); err != nil {
-	// 	log.Info("insertBalanceHandler --- ", err)
-	// }
-
-	// return "", nil
+	return users, nil
 }
