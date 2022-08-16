f4ee848c288bda7862d93cfbaa842ed2c9da335e
Boris <Zelyanin>
2022-08-16 10:32:44 +0300

    yes

--- internal/tools/genkey.go
+++ internal/tools/genkey.go
@@ -3,29 +3,10 @@ package tools
 import (
 	"crypto/aes"
 	"crypto/rand"
-	"crypto/sha256"
 	"fmt"
-	"os"
 
-	"github.com/itchyny/base58-go"
 	"golang.org/x/crypto/bcrypt"
 )
-
-func sha256Of(input string) []byte {
-	algorithm := sha256.New()
-	algorithm.Write([]byte(input))
-	return algorithm.Sum(nil)
-}
-
-func base58Encoded(bytes []byte) string {
-	encoding := base58.BitcoinEncoding
-	encoded, err := encoding.Encode(bytes)
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-	return string(encoded)
-}
 
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
